a7d0efb85f105d7cfdaf6ccf912b7e773febc417
Soni <nss911946@tatatechnologies.com>
2017-05-18 11:09:48 +0530

    Lecture 114, External Javascript

---
+++ 3-javascript/script.js
@@ -0,0 +1,50 @@
+var start = new Date().getTime();
+function getRandomColor()
+{
+    var letter='0123456789ABCDEF'.split('');
+    var color="#";
+    for (var i=0;i<6;i++)
+        {
+            color+=letter[Math.floor(Math.random()*16)];
+        }
+    return color;
+}
+function makeShapeAppear()
+{
+    var top=Math.random()*400;
+    var left=Math.random()*400;
+    var width=Math.random()*400;
+    var height=Math.random()*400;
+    document.getElementById("shape").style.borderRadius="0%";
+    if(Math.random()>0.5)
+    {
+        document.getElementById("shape").style.borderRadius="50%";
+        width=height;
+    }
+    else
+    {
+        document.getElementById("shape").style.borderRadius="0";    
+    }
+    var borderRadius=Math.random()*50;
+    document.getElementById("shape").style.top=top+"px";
+    document.getElementById("shape").style.left=left+"px";
+    document.getElementById("shape").style.width=width+"px";
+    document.getElementById("shape").style.height=height+"px";
+    document.getElementById("shape").style.backgroundColor=getRandomColor();
+    document.getElementById("shape").style.display="block";
+    start = new Date().getTime();
+}
+function appearAfterDelay()
+{
+    start = new Date().getTime();
+    setTimeout(makeShapeAppear,Math.random()*2000);
+}
+appearAfterDelay();
+document.getElementById("shape").onclick=function()
+{
+    document.getElementById("shape").style.display="none";
+    var end = new Date().getTime();
+    var timeTaken = (end - start)/1000;
+    document.getElementById("timeTaken").innerHTML=timeTaken+"s";
+    appearAfterDelay();
+}
